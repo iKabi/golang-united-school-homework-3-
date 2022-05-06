0f580b74da1de03f883c63427c6f778bd4b3f91e
Kabi <Kabilan_Shanmuga@epam.com>
2022-05-06 11:54:25 +0530

    Replace sort.Sort with sort.Ints

--- task03-map.go
+++ task03-map.go
@@ -1,13 +1,15 @@
 package homework
 
-import "sort"
+import (
+	"sort"
+)
 
 func sortMapValues(input map[int]string) (result []string) {
 	ks, vs := make([]int, 0, len(input)), make([]string, 0, len(input))
 	for k := range input {
 		ks = append(ks, k)
 	}
-	sort.Sort(sort.IntSlice(ks))
+	sort.Ints(ks)
 	for _, v := range ks {
 		vs = append(vs, input[v])
 	}
